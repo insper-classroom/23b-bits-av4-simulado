309ad3ea9164d373f4fe40cf35d9f0c18a4f08a8
Rafael Corsi <corsiferrao@gmail.com>
2023-11-06 10:49:33 -0300

    add code solution

--- Code.py
+++ Code.py
@@ -9,13 +9,48 @@ class testCode(VMCode):
         commands = []
         if command == "delete":
             commands.append("leaw $SP, %A")
+            commands.append("movw (%A), %A")
+            commands.append("subw %A, $1, %D")
+            commands.append("leaw $SP, %A")
+            commands.append("movw %D, (%A)")
 
         elif command == "add3":
             commands.append("leaw $SP, %A")
+            commands.append("movw (%A), %A")
+            commands.append("decw %A")
+            commands.append("movw (%A), %D, %D")
+            commands.append("decw %A")
+            commands.append("addw (%A), %D, %D")
+            commands.append("decw %A")
+            commands.append("addw (%A), %D, %D")
+            commands.append("movw %D, (%A)")
+            commands.append("addw %A, $1, %D")
+            commands.append("leaw $SP, %A")
+            commands.append("movw %D, (%A)")
 
         elif command == "swap":
             commands.append("leaw $SP, %A")
+            commands.append("movw (%A), %A")
+            commands.append("decw %A")
+            commands.append("movw (%A), %D")
+            commands.append("leaw $7, %A")
+            commands.append("movw %D, (%A)")
 
+            commands.append("leaw $SP, %A")
+            commands.append("movw (%A), %A")
+            commands.append("decw %A")
+            commands.append("decw %A")
+            commands.append("movw (%A), %D")
+            commands.append("incw %A")
+            commands.append("movw %D, (%A)")
+
+            commands.append("leaw $7, %A")
+            commands.append("movw (%A), %D")
+            commands.append("leaw $SP, %A")
+            commands.append("movw (%A), %A")
+            commands.append("decw %A")
+            commands.append("decw %A")
+            commands.append("movw %D, (%A)")
 
         # não mexer
         self.commandsToFile(commands)
